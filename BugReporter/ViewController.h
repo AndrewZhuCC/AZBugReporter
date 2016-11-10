92bb92cf561d56420245f119db38a9d9f40e1379
Andrew <zaz92537@126.com>
2016-11-10 15:01:31 +0800

    Initial Commit

---
+++ BugReporter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BugReporter
+//
+//  Created by 朱安智 on 2016/11/10.
+//  Copyright © 2016年 Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
